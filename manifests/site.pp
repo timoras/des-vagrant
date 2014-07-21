d1a7a2535c98c48341ca993e480ae330f82d6464
tomasz <tomasz@noemail.com>
2014-07-21 13:34:16 +0300

    db configed

---
+++ manifests/site.pp
@@ -0,0 +1,12 @@
+class { 'postgresql::server':
+  ip_mask_allow_all_users    => '0.0.0.0/0',
+  listen_addresses           => '*',
+  manage_firewall            => true,
+  postgres_password          => 'TPSrep0rt!',
+}
+
+
+postgresql::server::db { 'des_db':
+  user     => 'desuser',
+  password => 'desuser'
+}
